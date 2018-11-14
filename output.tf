a65e3dd3fa5ab3f85dba4f5fae0c129e7a1215d6
FireWorm <linked2u@gmail.com>
2018-11-14 17:56:05 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,17 @@
+# The following code will give us the zone_id of the AWS Route53 zone that is created by the configuration in our main.tf
+
 output "zone_id" {
   value = "${aws_route53_zone.primary.zone_id}"
 }
 
+# The following code will provide us with the FQDNs of the NameServers responsible for our AWS Route53 zone
+
 output "name_servers" {
   value = "${data.aws_route53_zone.selected.name_servers}"
 }
+
+# The following block of code will give us the IP Addresses of the NameServers responsible for our AWS Route53 zone. 
+# The addresses are resolved by the ```dns_a_record_set``` data source. 
 
 output "name_server_ip" {
   value = "${data.dns_a_record_set.visitbg_local_ns.*.addrs}"
