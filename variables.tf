cc9a3432793b41bd0df390e6f5d28086051a7867
FireWorm <linked2u@gmail.com>
2018-11-14 17:58:08 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+# The variables used by our Terraform configuration are defined here.
+# Their values are fetched from the ENV variables that we got set in the beginning. 
+
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "ami_id" {}
