89a6dc657bc8c1a969c2f743bce3639ed2ff7284
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2018-11-15 10:41:56 +0200

    updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,7 @@
-# The variables used by our Terraform configuration are defined here.
-# Their values are fetched from the ENV variables that we got set in the beginning. 
-
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "ami_id" {}
 variable "key_name" {}
 variable "region_id" {}
-variable "ssh_username" {}
-
 variable "vpc_security_group_ids" {
   type = "list"
 }
